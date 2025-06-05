64f6d76466bc42ba45551e352d1a1a80373f7165
hongh931 <hn582183@dal.ca>
2025-06-05 01:26:41 -0300

    refactor util, formalize visual1

--- Group C/rsfMRI/Utility/util.py
+++ Group C/rsfMRI/Utility/util.py
@@ -5,6 +5,6 @@ from typing import Sequence
 # root_name: The root name of the repository (dataset)
 # path: The specific
 #
-def get_mri_file_path(root_name, path: Sequence[str]):
+def get_mri_file_path(pre_path: Sequence[str], dataset_name, path: Sequence[str]):
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(base_dir, root_name, *path)
+    return os.path.join(base_dir, *pre_path, dataset_name, *path)
--- Group C/rsfMRI/visual1.py
+++ Group C/rsfMRI/visual1.py
@@ -2,21 +2,20 @@ import os
 import nibabel as nib
 from nilearn import plotting
 from util import get_mri_file_path
+from config import OUTPUT_ROOT
 import matplotlib
 matplotlib.use('MacOSX')  # option: TkAgg (Tkinter GUI), MacOSX
 
 # import dataset
-file_path = get_mri_file_path(root_name='MRIData',
+file_path = get_mri_file_path(pre_path=['..', '..', '..'],
+                              dataset_name='MRIData',
                               path=['sub-kaneff01', 'anat', 'sub-kaneff01_T1w.nii.gz'])
 img = nib.load(file_path)
-
-output_base = 'output'
-os.makedirs(output_base, exist_ok=True)
 
 step = 5  # save the figure every 5 layer
 
 # Iterate through the volume along the x-axis and save one sagittal slice every `step` layers
-x_dir = os.path.join(output_base, 'x-axis')
+x_dir = os.path.join(OUTPUT_ROOT, 'x-axis')
 os.makedirs(x_dir, exist_ok=True)
 for x in range(0, img.shape[0], step):
     fig = plotting.plot_anat(img,
@@ -27,7 +26,7 @@ for x in range(0, img.shape[0], step):
     fig.close()
 
 # Iterate through the volume along the y-axis and save one coronal slice every `step` layers
-y_dir = os.path.join(output_base, 'y-axis')
+y_dir = os.path.join(OUTPUT_ROOT, 'y-axis')
 os.makedirs(y_dir, exist_ok=True)
 for y in range(0, img.shape[1], step):
     fig = plotting.plot_anat(img,
@@ -38,7 +37,7 @@ for y in range(0, img.shape[1], step):
     fig.close()
 
 # Iterate through the volume along the z-axis and save one axial slice every `step` layers
-z_dir = os.path.join(output_base, 'z-axis')
+z_dir = os.path.join(OUTPUT_ROOT, 'z-axis')
 os.makedirs(z_dir, exist_ok=True)
 for z in range(0, img.shape[2], step):
     fig = plotting.plot_anat(img,
